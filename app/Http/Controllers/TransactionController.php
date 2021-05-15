853ad92e1b75a2a9df1e76019ca28b11dbc68e59
afifvdin <afifudin.fdn@gmail.com>
2021-05-15 07:39:04 +0700

    fix typo

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -8,21 +8,21 @@ class TransactionController extends Controller
 {
     // [VIEW/GET] localhost:8000/admin/transaction
     public function index() {
-        return view('Admin.Book.index');
+        return view('Admin.Transaction.index');
     }
 
     // [VIEW/GET] localhost:8000/admin/transaction/register
     public function register() {
-        return view('Admin.Book.register');
+        return view('Admin.Transaction.register');
     }
 
     // [VIEW/GET] localhost:8000/admin/transaction/:id
     public function detail() {
-        return view('Admin.Book.detail');
+        return view('Admin.Transaction.detail');
     }
 
     // [VIEW/GET] localhost:8000/admin/transaction/:id/edit
     public function edit() {
-        return view('Admin.Book.edit');
+        return view('Admin.Transaction.edit');
     }
 }
