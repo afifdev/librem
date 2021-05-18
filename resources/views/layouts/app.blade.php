f661c9fd71ef77414d63c102f32f6d293efa45c7
afifvdin <afifudin.fdn@gmail.com>
2021-05-18 10:29:19 +0700

    I Dont know to commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,30 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @yield('customStyle')
-    {{-- <link href="{{ asset('css/fitri/style.css') }}" rel="stylesheet"> --}}
+    @yield('header')
 </head>
 
 <body>
-    <div id="app">
-        {{-- Navigation --}}
-        @if (!auth()->guard('admin')->check())
-        @include('layouts.navigation')
-        @endif
-        <main>
-            @yield('content')
-        </main>
-    </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="{{ asset('js/jquery.js') }}"></script>
-    <script src="{{ asset('js/pooper.js') }}"></script>
+    @yield('content')
+    @yield('script')
     <script src="{{ asset('js/bootstrap.js') }}"></script>
 </body>
-
 </html>
