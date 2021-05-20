8c10a5d711b275819f5e4b06ac1087b2cd88c549
afifvdin <afifudin.fdn@gmail.com>
2021-05-20 11:03:37 +0700

    working on transaction view

---
+++ resources/views/Admin/Transaction/show.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @if (session()->has('error'))
+        <p>{{session()->get('error')}}</p>
+    @endif
+    <form action="{{route('trans_update', ['id' => $transaction->id])}}" method="POST">
+        @method('put')
+        @csrf
+        <p>Book Code</p>
+        <p>{{$transaction->book_code}}</p>
+        <p>Student NIS</p>
+        <p>{{$transaction->student_nis}}</p>
+        <p>Teacher NIP</p>
+        <p>{{$transaction->teacher_nip}}</p>
+        <p>Tipe</p>
+        <p>{{$transaction->detail->type}}</p>
+        <p>Take Date</p>
+        <p>{{$transaction->detail->take_date}}</p>
+        <p>Due Date</p>
+        <p>{{$transaction->detail->due_date}}</p>
+        <p>Return date</p>
+        @if (!$transaction->detail->return_date)
+            <input type="date" name="return_date">
+        @else
+            <p>{{$transaction->detail->return_date}}</p>
+        @endif
+        <p>Penalty</p>
+        @if(!$transaction->detail->penalty)
+            <p>0</p>        
+        @else
+            <p>{{$transaction->detail->penalty}}</p>        
+        @endif
+        <p>Uang yang sudah dibayar</p>
+        @if(!$transaction->detail->penalty)
+            <p>0</p>
+        @else
+            @if ($transaction->detail->penalty === $transaction->detail->debt_collected)
+                <p>{{$transaction->detail->debt_collected}}</p>
+            @else
+                <input type="text" name="debt_collected" value="{{$transaction->detail->debt_collected}}">
+            @endif
+        @endif
+        <p>Detail</p>
+        <p>{{$transaction->detail->detail}}</p>
+        <br><br>
+        <button type="submit">Update</button>
+    </form>
+    <br>
+    <form action="{{route('trans_delete', ['id' => $transaction->id])}}" method="POST">
+        @method('delete')
+        @csrf
+        <button type="submit">Delete</button>
+    </form>
+</body>
+</html>
